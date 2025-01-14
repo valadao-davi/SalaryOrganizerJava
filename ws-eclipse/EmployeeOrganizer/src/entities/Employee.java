91abadce7f23aff0ab486ed5204bc1136be91391
valadao-davi <davi.raichu@gmail.com>
2025-01-14 15:29:36 -0300

    feat: finish Employee class

--- ws-eclipse/EmployeeOrganizer/src/entities/Employee.java
+++ ws-eclipse/EmployeeOrganizer/src/entities/Employee.java
@@ -1,5 +1,35 @@
 package entities;
 
 public class Employee {
-
+	private String name;
+	private String email;
+	private double Salary;
+	
+	public Employee(String name, String email, double salary) {
+		this.name = name;
+		this.email = email;
+		Salary = salary;
+	}
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public double getSalary() {
+		return Salary;
+	}
+	public void setSalary(double salary) {
+		Salary = salary;
+	}
+	
+	
 }
