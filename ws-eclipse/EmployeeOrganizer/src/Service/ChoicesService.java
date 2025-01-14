0089544cfacaf212e1461f0b10f723f25610a629
valadao-davi <davi.raichu@gmail.com>
2025-01-14 16:02:04 -0300

    feat: start of ChoicesService

--- ws-eclipse/EmployeeOrganizer/src/Service/ChoicesService.java
+++ ws-eclipse/EmployeeOrganizer/src/Service/ChoicesService.java
@@ -6,5 +6,18 @@ import entities.Employee;
 
 public class ChoicesService {
 	private List<Employee> listProducts;
+	
+	public ChoicesService(List<Employee> listProducts) {
+		super();
+		this.listProducts = listProducts;
+	}
+
+	public List<Employee> getListProducts() {
+		return listProducts;
+	}
+
+	public void setListProducts(List<Employee> listProducts) {
+		this.listProducts = listProducts;
+	}
 
 }
