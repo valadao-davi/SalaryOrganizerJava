430136fc80a27c08c6f9baf65d4542021b85909d
valadao-davi <davi.raichu@gmail.com>
2025-01-14 16:33:26 -0300

    refactor: made the choice interface a function of ServiceClass

--- ws-eclipse/EmployeeOrganizer/src/Service/ChoicesService.java
+++ ws-eclipse/EmployeeOrganizer/src/Service/ChoicesService.java
@@ -1,6 +1,7 @@
 package Service;
 
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 import entities.Employee;
@@ -48,7 +49,7 @@ public class ChoicesService {
 	public List<String> showNameAscending(boolean salary){
 		if(salary) {
 			return listEmployee.stream()
-					.sorted((e1,e2)-> Double.compare(e1.getSalary(), e2.getSalary()))
+					.sorted((e1,e2)-> -Double.compare(e1.getSalary(), e2.getSalary()))
 					.map((e)-> e.getName())
 					.collect(Collectors.toList());
 		}
@@ -56,6 +57,54 @@ public class ChoicesService {
 				.map(e -> e.getName())
 				.sorted((s1,s2)-> s1.compareTo(s2))
 				.collect(Collectors.toList());
+	}
+	
+	public void choiceInterface() {
+		Scanner sc = new Scanner(System.in);
+		boolean running = false;
+		while(running) {
+			System.out.println("\nSelect an option:");
+            System.out.println("1. Show emails of employees with salary below a specified value");
+            System.out.println("2. Show sum of salaries of employees with names starting with a given initial");
+            System.out.println("3. Show salaries in descending order");
+            System.out.println("4. Show employees sorted by name (ascending)");
+            System.out.println("5. Show employees sorted by salary (descending)");
+            System.out.println("6. Exit");
+            
+            int choice = sc.nextInt();
+            sc.nextLine();
+            
+            switch(choice) {
+            	case 1:
+            		System.out.println("EMAIL FILTERS BASED OF SALARY SERVICE: ");
+            		System.out.println("Insert a salary to threshold to display employee emails: ");
+        			double filterValue = sc.nextDouble();
+        			List<String> emailsFiltered = this.emailsBelowSalary(filterValue);
+        			emailsFiltered.forEach(System.out::println);
+        			break;
+            	case 2:
+            		System.out.println("SUM SALARY BASED OF INITIAL SERVICE: ");
+            		System.out.println("Insert a letter to find the sum of people with this initial: ");
+        			char nameInitial = sc.next().toUpperCase().charAt(0);
+        			double sumInitial = this.sumBasedInitial(nameInitial);
+        			System.out.println("Sum of salary of people whose name starts with " + nameInitial + ": $" + sumInitial);
+        			break;
+            	case 3:
+            		System.out.println("SALARIES IN DESCENDING ORDER SERVICE: ");
+            		List<Double> salaries = this.showSalaryEmployees();
+            		salaries.forEach((s) -> System.out.println("$" + s));
+            		break;
+            	case 4:
+            		System.out.println("EMPLOYEES SORTED BY NAME SERVICE: ");
+            		List<String> names = this.showNameAscending(false);
+            		names.forEach(System.out::println);
+            		break;
+            	case 5:
+            		System.out.println("EMPLOYEES SORTED BY SALARY SERVICE: ");
+            		List<String> namesSalary = this.showNameAscending(true);
+            		namesSalary.forEach(System.out::println);
+            }
+		}
 	}
 
 }
