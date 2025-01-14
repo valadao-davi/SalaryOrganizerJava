8b557f9507213e8ea226bd575c8e79467ee66a28
valadao-davi <davi.raichu@gmail.com>
2025-01-14 15:33:44 -0300

    feat: implemented file reader try-catch

--- ws-eclipse/EmployeeOrganizer/src/application/Program.java
+++ ws-eclipse/EmployeeOrganizer/src/application/Program.java
@@ -1,5 +1,8 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 public class Program {
@@ -10,6 +13,11 @@ public class Program {
 		System.out.println("Set the path of the project: ");
 		String path = "c:\\temp\\employees.csv";
 		
+		try(BufferedReader br = new BufferedReader(new FileReader(path))){
+			
+		}catch(IOException e) {
+			System.out.println("Ocurred an error while trying to read the archive: " + e.getMessage());
+		}
 		
 		sc.close();
 	}
